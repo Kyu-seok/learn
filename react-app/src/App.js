d2c1ce5508970c1bcadbfdb484ecc21a0b80f354
yeumkyuseok <yeumkyuseok@gmail.com>
2021-06-30 20:09:28 +0900

    added create functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import React, { Component } from "react";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: "read",
       selected_content_id: 2,
@@ -23,32 +27,78 @@ class App extends Component {
       ],
     };
   }
-  render() {
-    console.log("App redner");
-    var _title,
-      _desc = null;
-    if (this.state.mode === "welcome") {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if (this.state.mode === "read") {
-      var i = 0;
-      while(i < this.state.contents.length) {
+  getReadContent(){
+    var i = 0;
+      while (i < this.state.contents.length) {
         var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-      _title = data.title;
-      _desc = data.desc;
+        if (data.id === this.state.selected_content_id) {
+          return data;
+          break;
         }
         i = i + 1;
       }
+  }
+  getContent() {
+    console.log("App redner");
+    var _title,
+      _desc,
+      _article = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "read") {
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            // add content to
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+            });
+            console.log(_title, _desc);
+          }.bind(this)}
+        ></CreateContent>
+      );
+    } else if (this.state.mode === "update") {
+      var _content = this.getReadContent();
+      _article = (
+        <UpdateContent
+          data={_content} onSubmit={function (_title, _desc) {
+            // add content to
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+            });
+            console.log(_title, _desc);
+          }.bind(this)}
+        ></UpdateContent>
+      );
     }
+    return _article;
+  }
+  render() {
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
           onChangePage={function () {
-            this.setState({ 
-              mode: "welcome"
+            this.setState({
+              mode: "welcome",
             });
           }.bind(this)}
         ></Subject>
@@ -56,12 +106,19 @@ class App extends Component {
           onChangePage={function (id) {
             this.setState({
               mode: "read",
-              selected_content_id: Number(id)
+              selected_content_id: Number(id),
             });
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode,
+            });
+          }.bind(this)}
+        ></Control>
+        {this.getContent()}
       </div>
     );
   }
