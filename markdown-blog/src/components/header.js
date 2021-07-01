4f51f0a9d5abe25d70cf9e8eb7da1c00bfb7f16c
yeumkyuseok <yeumkyuseok@gmail.com>
2021-07-02 07:10:28 +0900

    design added to header

--- markdown-blog/src/components/header.js
+++ markdown-blog/src/components/header.js
@@ -1,12 +1,52 @@
-import React from "react"
-import "./components.css"
+import React from "react";
+import "./components.css";
+import { Link } from "react-router-dom";
+import avatar from "../images/engineer.png";
+import githubIcon from "../images/github.png";
+import IgIcon from "../images/instagram.png";
+
+const user = {
+  githubLink: "https://github.com/yeumkyuseok",
+  igLink: "https://www.instagram.com/kyuseok_y/",
+};
 
 const Header = () => {
-    return (
-        <div className="header">
-            <h1>Your favourite boy Q's blog</h1>
-        </div>
-    )
-}
+  return (
+    <header className="site-header">
+      <div className="branding">
+        <img className="avatar" src={avatar} alt="" />
+        <p className="header">
+          <Link id="header-title" to="/">
+            Q's blog
+          </Link>
+        </p>
+      </div>
+      <nav className="site-nav">
+        <ul>
+          <li>
+            <Link className="links" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <a href={user.githubLink}>
+              <img className="icon-button" src={githubIcon} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href={user.igLink}>
+              <img
+                className="icon-button"
+                href="https://www.instagram.com/kyuseok_y/"
+                src={IgIcon}
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
