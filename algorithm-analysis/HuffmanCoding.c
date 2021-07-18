43b0d86a5295b67d41e63f071797bb92951741b9
yeumkyuseok <yeumkyuseok@gmail.com>
2021-07-18 22:49:12 +0900

    added work

---
+++ algorithm-analysis/HuffmanCoding.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Huffman coding is an algorith that is used to compress data
+// Huffman coding uses hashing method to incode the data
+
+int main(void) {
+
+    // implement huffman coding algorithm here.
+
+    return 0;
+}
