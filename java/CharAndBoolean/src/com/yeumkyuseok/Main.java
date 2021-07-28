fc70a9db8ca02c36982e086af1a571449c1979bd
yeumkyuseok <yeumkyuseok@gmail.com>
2021-07-29 01:29:27 +0900

    recap on java primitive datatype

---
+++ java/CharAndBoolean/src/com/yeumkyuseok/Main.java
@@ -0,0 +1,19 @@
+package com.yeumkyuseok;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myKoreanChar = '\ub098';
+        System.out.println(myKoreanChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
