53ab75485f788901daa8873a95aa7383c2590a3d
yeumkyuseok <yeumkyuseok@gmail.com>
2021-07-29 18:06:52 +0900

    recap on java OOP basics

---
+++ java/Strings/src/com/yeumkyuseok/Main.java
@@ -0,0 +1,22 @@
+package com.yeumkyuseok;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        String myString = "This is a string";
+        System.out.println("myString is equal to " +  myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString =" \ub059";
+        System.out.println("myString is equal to " + myString);
+
+    }
+}
