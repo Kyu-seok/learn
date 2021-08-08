bce64ac5d0c53f8ba5dc603db8dc395be72732ed
yeumkyuseok <yeumkyuseok@gmail.com>
2021-08-08 23:08:35 +0900

    Initialized Top10Download project

---
+++ android-development/Top10Downloader/app/src/main/java/com/yeumkyuseok/top10downloader/MainActivity.java
@@ -0,0 +1,31 @@
+package com.yeumkyuseok.top10downloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        while(true) {
+            int x = 1;
+        }
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+}
