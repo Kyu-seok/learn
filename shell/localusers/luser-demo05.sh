7df56e15eaf0a2e36edd669cec5ab34cc0626f59
yeumkyuseok <yeumkyuseok@gmail.com>
2021-09-24 11:12:39 +0900

    generating number feature added to the script

--- shell/localusers/luser-demo05.sh
+++ shell/localusers/luser-demo05.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# This script generates a list of random passwords.
+
+# A random number as a password.
+PASSWORD="${RANDOM}"
+echo "${PASSWORD}"
+
+# Three random numbers together.
+PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
+echo "${PASSWORD}"
