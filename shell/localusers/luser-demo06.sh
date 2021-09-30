d3027e253513a00d81e889b8ae80a358fb9cbf2d
yeumkyuseok <yeumkyuseok@gmail.com>
2021-09-30 14:09:40 +0900

    displaying path and file name feature added to the script.

--- shell/localusers/luser-demo06.sh
+++ shell/localusers/luser-demo06.sh
@@ -3,3 +3,8 @@
 # This script generates a random password for each user specified on the command line.
 
 # Display what the user typed on the command line.
+echo "You executed this command: ${0}"
+
+# Display the path and the filename of the script.
+echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script."
+
