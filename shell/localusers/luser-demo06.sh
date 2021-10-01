5ba7a55dcf13d7e1058cd7bc5eec5aefe7aaf7eb
yeumkyuseok <yeumkyuseok@gmail.com>
2021-10-01 14:31:33 +0900

    for loop,  feature implemented to the script

--- shell/localusers/luser-demo06.sh
+++ shell/localusers/luser-demo06.sh
@@ -13,4 +13,17 @@ echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script."
 NUMBER_OF_PARAMETERS="${#}"
 echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line."
 
+# Make user they at least supply one argument.
+if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]
+then
+  echo "Usage: ${0} USER_NAME [USER_NAME]..."
+  exit 1
+fi
+
+# Generate and display a password for each parameter.
+for USER_NAME in "${@}"
+do 
+  PASSWORD=$(date +%s%N | sha256sum | head -c48)
+  echo "${USER_NAME}: ${PASSWORD}"
+done
 
