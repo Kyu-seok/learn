c1cf4dcbc54d99b16179820805318d64d0ab6369
yeumkyuseok <yeumkyuseok@gmail.com>
2021-10-01 14:16:31 +0900

    counting number of parameters feature added to the script

--- shell/localusers/luser-demo06.sh
+++ shell/localusers/luser-demo06.sh
@@ -8,3 +8,9 @@ echo "You executed this command: ${0}"
 # Display the path and the filename of the script.
 echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script."
 
+# Tell them how many arguments they passed int.
+# (Inside the script they are parameters, outside they are arguments.)
+NUMBER_OF_PARAMETERS="${#}"
+echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line."
+
+
