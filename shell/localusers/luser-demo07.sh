a4e1127c4a6f58a2e788fb0f88a29df0765e895a
yeumkyuseok <yeumkyuseok@gmail.com>
2021-10-03 14:57:47 +0900

    while loop and shift feature added to the script

--- shell/localusers/luser-demo07.sh
+++ shell/localusers/luser-demo07.sh
@@ -3,3 +3,19 @@
 # Demonstrrate the use of shift and while loops.
 
 # Display the first three paramenters.
+echo "Initial number of parameters: ${#}"
+echo "Parameter 1: ${1}"
+echo "Parameter 2: ${2}"
+echo "Parameter 3: ${3}"
+echo 
+
+# Loop through all the positional parameters.
+while [[ "${#}" -gt 0 ]]
+do 
+  echo "Number of parameters: ${#}"
+  echo "Parameter 1: ${1}"
+  echo "Parameter 2: ${2}"
+  echo "Parameter 3: ${3}"
+  echo 
+  shift 
+done
