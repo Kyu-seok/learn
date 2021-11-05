7d6f6243efc2d93fb2de56bae2e115ea590273a4
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-06 03:05:06 +0900

    implemented spring bean scopes and lifecycle methods using java annotations

--- spring/spring-demo-annotation/src/main/java/com/yeumkyuseok/springdemo/TennisCoach.java
+++ spring/spring-demo-annotation/src/main/java/com/yeumkyuseok/springdemo/TennisCoach.java
@@ -1,11 +1,17 @@
 package com.yeumkyuseok.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -13,11 +19,25 @@ public class TennisCoach implements Coach{
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff() ");
+    }
+
+    /*
     @Autowired
     public void doSomeCrazyStuff(FortuneService fortuneService) {
         System.out.println(">> TennisCoach: inside setFortuneService() method");
         this.fortuneService = fortuneService;
     }
+    */
 
     /*
     @Autowired
