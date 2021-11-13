8049d74a30f3b3021ccd4572aef7faea2c6f9a0a
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-13 12:33:39 +0900

    Implemented one to many mapping

---
+++ spring/hb-03-one-to-many/src/main/java/com/yeumkyuseok/hibernate/demo3/CreateInstructorDemo.java
@@ -0,0 +1,56 @@
+package com.yeumkyuseok.hibernate.demo3;
+
+import com.yeumkyuseok.hibernate.demo3.entity.Course;
+import com.yeumkyuseok.hibernate.demo3.entity.Instructor;
+import com.yeumkyuseok.hibernate.demo3.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class CreateInstructorDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            Instructor tempInstructor = new Instructor("Susan", "Public", "susan.public@luv2code.com");
+
+            InstructorDetail tempInstructorDetail = new InstructorDetail(
+                    "http://www.youtube.com",
+                    "Video Games");
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            System.out.println("Saving instructor: " + tempInstructor);
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
