0afc984a96ef1bb3a94afe9703fef99cd10b06bd
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-15 07:28:21 +0900

    Initialized web customer tracker

---
+++ spring/web-customer-tracker/src/main/java/com/example/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.example.springdemo.dao;
+
+import com.example.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+}
