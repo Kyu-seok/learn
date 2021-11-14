48386f968b302c7292ac9d14bd40f994cdb8c1c7
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-14 09:29:25 +0900

    Implemented many to many mappings between course, and student

---
+++ spring/hb-05-many-to-many/src/main/java/com/yeumkyuseok/hibernate/demo5/GetCoursesForMaryDemo.java
@@ -0,0 +1,49 @@
+package com.yeumkyuseok.hibernate.demo5;
+
+import com.yeumkyuseok.hibernate.demo5.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class GetCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student mary from database
+            int studentId = 2;
+            Student tempStudent = session.get(Student.class, studentId);
+            System.out.println("\nLoaded student: " + tempStudent);
+            System.out.println("Course: " + tempStudent.getCourses());
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
