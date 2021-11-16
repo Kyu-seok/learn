7b840e12eb0dee9d8fccd4c791589f084454be75
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-16 11:10:11 +0900

    Initialized and implemented @Pointcut declaration

---
+++ spring/spring-demo-aop-pointcut-declarations/src/main/java/com/yeumkyuseok/aopdemopointcut/dao/AccountDAO.java
@@ -0,0 +1,20 @@
+package com.yeumkyuseok.aopdemopointcut.dao;
+
+import com.yeumkyuseok.aopdemopointcut.Account;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount(Account theAccount, boolean vipFlag) {
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+    }
+
+    public boolean doWork() {
+
+        System.out.println(getClass() + ": doWork()");
+
+        return false;
+    }
+
+}
