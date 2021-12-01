efbe2d4461715e79774359d682cb8eaf70138fe3
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-01 20:19:36 +0900

    Implemented REST API for getting student with index ID

--- spring/spring-rest-demo/src/main/java/com/yeumkyuseok/springdemo/rest/StudentRestController.java
+++ spring/spring-rest-demo/src/main/java/com/yeumkyuseok/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.yeumkyuseok.springdemo.rest;
 
 import com.yeumkyuseok.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,32 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+
     // define endpoint for "/students" - return list of students
     @GetMapping("/students")
     public List<Student> getStudent() {
 
-        List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("Poornima", "Patel"));
-        theStudents.add(new Student("Mario", "Rossi"));
-        theStudents.add(new Student("Mary", "Smith"));
-
         return theStudents;
+    }
+
+    // define the endpoint for "/studetns/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
