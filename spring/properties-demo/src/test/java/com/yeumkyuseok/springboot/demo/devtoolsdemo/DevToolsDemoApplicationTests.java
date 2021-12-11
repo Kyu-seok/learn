8a346ca055a719c8880b808e4f88825d75657005
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-11 23:15:06 +0900

    Implemented properties-demo project

---
+++ spring/properties-demo/src/test/java/com/yeumkyuseok/springboot/demo/devtoolsdemo/DevToolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeumkyuseok.springboot.demo.devtoolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToolsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
