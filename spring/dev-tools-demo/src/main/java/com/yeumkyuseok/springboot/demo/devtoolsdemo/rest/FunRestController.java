ba777a9307e253f85682543bf19f8e83ae664638
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-11 20:39:47 +0900

    APIs added for /workout and /fortune

--- spring/dev-tools-demo/src/main/java/com/yeumkyuseok/springboot/demo/devtoolsdemo/rest/FunRestController.java
+++ spring/dev-tools-demo/src/main/java/com/yeumkyuseok/springboot/demo/devtoolsdemo/rest/FunRestController.java
@@ -14,4 +14,15 @@ public class FunRestController {
         return "Hello World! Time on server is " + LocalDateTime.now();
     }
 
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5km!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
 }
