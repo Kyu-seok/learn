1f0c77fe521fb8f5ef09d6551c0083c711f620b3
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-18 23:16:33 +0900

    Implemented update button and functionality

--- spring/thymeleafdemo/src/main/java/com/yeumkyuseok/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring/thymeleafdemo/src/main/java/com/yeumkyuseok/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,11 +5,8 @@ import com.yeumkyuseok.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,8 +22,28 @@ public class EmployeeController {
 
     @GetMapping("/list")
     public String listEmployees(Model theModel) {
-        List <Employee> theEmployees = employeeService.findAll();
+        List<Employee> theEmployees = employeeService.findAll();
         theModel.addAttribute("employees", theEmployees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        Employee theEmployee = employeeService.findById(theId);
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
     }
 }
--- spring/thymeleafdemo/src/main/java/com/yeumkyuseok/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ spring/thymeleafdemo/src/main/java/com/yeumkyuseok/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.yeumkyuseok.springboot.thymeleafdemo.dao;
 import com.yeumkyuseok.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
