b02016b8712be1bce3004d7193532ef5ed20f453
yeumkyuseok <yeumkyuseok@gmail.com>
2022-01-25 23:35:58 +0900

    BMI coding challenge added

--- javascript/Fundamentals/script.js
+++ javascript/Fundamentals/script.js
@@ -2,7 +2,7 @@ let js = 'amazing';
 // console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
-conlose.log("kyu");
+console.log("kyu");
 console.log(23);
 
 let firstName = "Matilda";
@@ -55,7 +55,6 @@ console.log(ageJonas, ageSarah);
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3, 2 * 2 * 2
 
-const firstName = 'Jonas';
 const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
 
@@ -68,34 +67,17 @@ console.log(x);
 
 console.log(ageJonas > ageSarah);
 
-// Presedence of operators
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y);
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn
+console.log(BMIMark, BMIJohn, markHigherBMI);
