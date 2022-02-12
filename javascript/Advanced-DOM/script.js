b740e00718bf18f348f9a3e4dfffc4fd01dfb0e4
KYU <kyu.certified@gmail.com>
2022-02-12 12:51:16 +0900

    Implemented Menu fade animation using handler

--- javascript/Advanced-DOM/script.js
+++ javascript/Advanced-DOM/script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -82,11 +86,6 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     }
 });
 
-// Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 tabsContainer.addEventListener('click', function(e) {
     const clicked = e.target.closest('.operations__tab');
@@ -101,6 +100,24 @@ tabsContainer.addEventListener('click', function(e) {
     // Active content area
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 })
+
+// Menu fade animation
+const handleHover = function(e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+}
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ///////////////////////////////////////
 ///////////////////////////////////////
