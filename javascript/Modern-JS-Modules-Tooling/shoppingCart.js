295f32ab904f8d5da3f99a2dad7eceb92dd8c119
Kyu <yeumkyuseok@gmail.com>
2022-03-10 15:47:46 +0900

    Implement Module Pattern

--- javascript/Modern-JS-Modules-Tooling/shoppingCart.js
+++ javascript/Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting module
 console.log('Exporting module');
 
+// Blocking code
+// console.log('Start fetching users');
+// const res = await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 
@@ -18,3 +23,4 @@ export default function(product, quantity) {
     cart.push({product, quantity});
     console.log(`${quantity} ${product} added to cart`);
 }
+
