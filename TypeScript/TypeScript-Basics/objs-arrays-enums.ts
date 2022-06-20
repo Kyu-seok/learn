4d54e19897f47709ef425e7f0562c08926da9dde
Kyu-seok <yeumkyuseok@gmail.com>
2022-06-20 23:23:56 +0900

    Type examples added

---
+++ TypeScript/TypeScript-Basics/objs-arrays-enums.ts
@@ -0,0 +1,41 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Kyu',
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+  name: 'Kyu',
+  age: 25,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is ADMIN');
+}
