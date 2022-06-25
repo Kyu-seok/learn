19447c5fd90e2c1091b414c6e9d29d2ea488bde3
Kyu-seok <yeumkyuseok@gmail.com>
2022-06-25 20:28:50 +0900

    Initialize react-redux-counter-app

--- React/react-redux-counter-app/src/components/Header.js
+++ React/react-redux-counter-app/src/components/Header.js
@@ -1,35 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { authActions } from '../store/auth';
 import classes from './Header.module.css';
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const logoutHandler = (event) => {
-    event.preventDefault();
-    dispatch(authActions.logout());
-  }
-
-  const navigationComponent = (
-    <nav>
-      <ul>
-        <li>
-          <a href='/'>My Products</a>
-        </li>
-        <li>
-          <a href='/'>My Sales</a>
-        </li>
-        <li>
-          <button onClick={logoutHandler}>Logout</button>
-        </li>
-      </ul>
-    </nav>
-  );
-
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      {isAuthenticated && navigationComponent}
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>My Products</a>
+          </li>
+          <li>
+            <a href='/'>My Sales</a>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
