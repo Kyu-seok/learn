58fcbdf935a4c5457247a5290083d12b324526d8
Kyu-seok <yeumkyuseok@gmail.com>
2022-09-06 13:10:05 +0800

    Initialize Spring5WebbApp project

---
+++ spring/spring-guru/spring5webapp/src/test/java/com/kyu/spring5webapp/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyu.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
