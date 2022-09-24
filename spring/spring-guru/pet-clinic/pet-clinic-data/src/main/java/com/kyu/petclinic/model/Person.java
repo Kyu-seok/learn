4ea1c16eae99b8e6aff750c44840b1b799c8a308
Kyu-seok <yeumkyuseok@gmail.com>
2022-09-24 23:06:29 +0800

    Implement BaseEntity

--- spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/model/Person.java
+++ spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.kyu.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/model/PetType.java
+++ spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.kyu.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
