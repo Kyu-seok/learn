d0ac2a766a0ef0c56947735dfa843008c6471f16
Kyu-seok <yeumkyuseok@gmail.com>
2022-09-30 17:16:41 +0800

    Refactor services by implementing parent interface

--- spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/services/PetService.java
+++ spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/services/PetService.java
@@ -2,14 +2,6 @@ package com.kyu.petclinic.services;
 
 import com.kyu.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
