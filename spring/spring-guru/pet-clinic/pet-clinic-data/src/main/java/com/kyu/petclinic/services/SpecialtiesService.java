bd4ad8b8e51ae071f3922652c740fc10aebfd05e
Kyu-seok <yeumkyuseok@gmail.com>
2022-10-21 16:44:33 +0800

    Add Specialties Map Impl

---
+++ spring/spring-guru/pet-clinic/pet-clinic-data/src/main/java/com/kyu/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.kyu.petclinic.services;
+
+import com.kyu.petclinic.model.Specialty;
+
+import java.util.Set;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
