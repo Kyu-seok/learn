eef5a66f1c2864fd7728b6a94515eafc43758509
Kyu-seok <yeumkyuseok@gmail.com>
2022-11-16 12:19:12 +0900

    Implement showing error data on 404 error page

--- spring/spring-guru/spring5-recipe-app/src/main/java/com/kyu/spring5recipeapp/controllers/RecipeController.java
+++ spring/spring-guru/spring5-recipe-app/src/main/java/com/kyu/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.kyu.spring5recipeapp.controllers;
 
 import com.kyu.spring5recipeapp.commands.RecipeCommand;
+import com.kyu.spring5recipeapp.exceptions.NotFoundException;
 import com.kyu.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -49,7 +52,22 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
 
         recipeService.deleteById(Long.valueOf(id));
-        return "redirect:/";
 
+        return "redirect:/";
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
 }
