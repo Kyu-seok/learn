dc02b09af0f0458eaec4f87443571fd6ff909c74
Kyu-seok <yeumkyuseok@gmail.com>
2022-11-18 12:39:53 +0900

    Implement ControllerExceptionHandler.java to handle NumberFormatException to all controllers globally

--- spring/spring-guru/spring5-recipe-app/src/test/java/com/kyu/spring5recipeapp/controllers/ImageControllerTest.java
+++ spring/spring-guru/spring5-recipe-app/src/test/java/com/kyu/spring5recipeapp/controllers/ImageControllerTest.java
@@ -39,7 +39,9 @@ class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
 
     }
 
@@ -97,6 +99,13 @@ class ImageControllerTest {
         byte[] responseBytes = response.getContentAsByteArray();
 
         assertEquals(s.getBytes().length, responseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
 }
