74d0dc25a6c1e6a8a1c787310b6eefdba275f77b
Kyu-seok <yeumkyuseok@gmail.com>
2023-05-04 15:57:36 +0900

    Implement sequence operators in python

---
+++ Python/HelloWorld/sequence_operators.py
@@ -0,0 +1,9 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("hello " * 5)
